e5e07279506b55145d5d9d12bf21b0af1231c7de
NinJa <ninja@NinJadeMacBook-Pro.local>
2022-02-09 17:12:33 +0800

    first commit

---
+++ apps/fe-admin/src/views/Home.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Author: NinJa
+ * @Date: 2022-02-09 16:24:23
+ * @LastEditTime: 2022-02-09 16:34:21
+ * @LastEditors: NinJa
+ * @Description: 
+ * @FilePath: /data-screen/Volumes/T7/code/fe-interview/apps/fe-admin/src/views/Home.vue
+-->
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="fe-admin" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
---
+++ apps/fe-web/src/views/Home.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Author: NinJa
+ * @Date: 2022-02-09 16:18:59
+ * @LastEditTime: 2022-02-09 16:34:32
+ * @LastEditors: NinJa
+ * @Description: 
+ * @FilePath: /data-screen/Volumes/T7/code/fe-interview/apps/fe-web/src/views/Home.vue
+-->
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="fe-web" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
